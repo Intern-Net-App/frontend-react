0eca14bfe686bd38d9b3b9400d3c229991b5e4a5
ShernanJ <shernanjavier@gmail.com>
2023-10-23 21:00:16 -0400

    Implemented job posting retrieval

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Job {
+  ID: string;
+  JobTitle: string;
+  CompanyName: string;
+  CompanyLocation: string;
+  CompanyLink: string;
+  JobDetailUrl: string;
+  JobListed: string;
 }
 
-export default App
+function App() {
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const fetchJobs = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`http://localhost:8080/api/jobs?skip=${(page - 1) * 3}&limit=10`);
+      const newJobs: Job[] = response.data;
+      setJobs((prevJobs) => [...prevJobs, ...newJobs]);
+      setPage(page + 3);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      const options = {
+        threshold: 0.6,
+      };
+      const observer = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            fetchJobs();
+          }
+        },
+        options
+      );
+
+      if (containerRef.current) {
+        observer.observe(containerRef.current);
+      }
+
+      return () => {
+        if (containerRef.current) {
+          observer.unobserve(containerRef.current);
+        }
+      };
+    }
+  }, [loading]);
+
+  return (
+    <main style={{marginLeft: '20px'}}>
+      <h1>Job Postings</h1>
+      <div>
+        {jobs.map((job) => (
+          <div key={job.ID}>
+            <h2>{job.JobTitle}</h2>
+            <p>{job.CompanyName}</p>
+            <p>{job.CompanyLocation}</p>
+            <a href={job.JobDetailUrl} target='_blank'>Job Details</a>
+          </div>
+        ))}
+      </div>
+      <div ref={containerRef} style={{ height: '20px' }}>
+        {loading && <p>Loading...</p>}
+      </div>
+    </main>
+  );
+}
+
+export default App;
